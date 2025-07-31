965909b4da4e3ac05955da93289efcaba4e15808
datptph28194 <datptph28194@fpt.edu.vn>
2025-07-31 21:36:15 +0700

    update gói + billing 8.0.0

--- purchase/src/main/java/com/example/purchase/utils/PurchaseManager.kt
+++ purchase/src/main/java/com/example/purchase/utils/PurchaseManager.kt
@@ -36,7 +36,6 @@ object PurchaseManager {
         PurchasesUpdatedListener { _, _ -> }
 
 
-
     private val pendingParams: PendingPurchasesParams = PendingPurchasesParams.newBuilder().build()
 
     @JvmStatic
@@ -70,7 +69,7 @@ object PurchaseManager {
                         mBillingClient?.queryPurchasesAsync(
                             params2
                         ) { _, purchasesSub ->
-                            if (checkTimeIap(context)|| purchases.isNotEmpty()) {
+                            if (checkTimeIap(context) || purchases.isNotEmpty()) {
                                 isBought = true
                                 isEnableAds = false
                                 for (purchase in purchasesSub) {
@@ -198,23 +197,24 @@ object PurchaseManager {
     ) {
         mListKey = listKey.sortedBy { it }
         mBillingClient =
-            BillingClient.newBuilder(context).enablePendingPurchases(pendingParams).setListener { p0, p1 ->
-                if (p0.responseCode == BillingClient.BillingResponseCode.OK && p1 != null) {
-                    for (purchase in p1) {
-                        handlePurchase2(purchase)
+            BillingClient.newBuilder(context).enablePendingPurchases(pendingParams)
+                .setListener { p0, p1 ->
+                    if (p0.responseCode == BillingClient.BillingResponseCode.OK && p1 != null) {
+                        for (purchase in p1) {
+                            handlePurchase2(purchase)
+                        }
+                        isBought = true
+                        isEnableAds = false
+                        if (!isSub) {
+                            setCurrentTimeBoughtIap(context)
+                            setCurrentKeyIap(context, key = currentProductId)
+                        }
+                        queryIap.updatePurchase()
+                    } else {
+                        isBought = false
+                        isEnableAds = true
                     }
-                    isBought = true
-                    isEnableAds = false
-                    if (!isSub) {
-                        setCurrentTimeBoughtIap(context)
-                        setCurrentKeyIap(context, key = currentProductId)
-                    }
-                    queryIap.updatePurchase()
-                } else {
-                    isBought = false
-                    isEnableAds = true
-                }
-            }.build()
+                }.build()
         mBillingClient?.startConnection(object : BillingClientStateListener {
             override fun onBillingServiceDisconnected() {
 //                queryIap.queryFail()
@@ -233,7 +233,8 @@ object PurchaseManager {
                     }
                     val params = QueryProductDetailsParams.newBuilder()
                     params.setProductList(productList)
-                    mBillingClient?.queryProductDetailsAsync(params.build()
+                    mBillingClient?.queryProductDetailsAsync(
+                        params.build()
                     ) { p0, p1 ->
                         val productDetailsList = p1.productDetailsList
                         if (!p1.productDetailsList.isNullOrEmpty()) {
@@ -335,15 +336,9 @@ object PurchaseManager {
         val currentTime = System.currentTimeMillis()
         val timeBought = getCurrentTimeBoughtIap(context = context)
         if (isIapTest) {
-            return if (getCurrentKeyIap(context).endsWith("0.5")) {
-        if (isIapTest) {
-            return if(getCurrentKeyIap(context).endsWith("0.3")){
-                val currentTime = System.currentTimeMillis()
-                val timeBought = getCurrentTimeBoughtIap(context = context)
+            return if (getCurrentKeyIap(context).endsWith("0.3")) {
                 TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 1
-            }else if (getCurrentKeyIap(context).endsWith("0.5")) {
-                val currentTime = System.currentTimeMillis()
-                val timeBought = getCurrentTimeBoughtIap(context = context)
+            } else if (getCurrentKeyIap(context).endsWith("0.5")) {
                 TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 1
             } else if (getCurrentKeyIap(context).endsWith("1")) {
                 TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 2
@@ -360,81 +355,78 @@ object PurchaseManager {
                 false
             }
         } else {
-            return if (getCurrentKeyIap(context).endsWith("0.5")) {
-                TimeUnit.MILLISECONDS.toDays(currentTime - timeBought) <= 1
-            return if(getCurrentKeyIap(context).endsWith("0.3")){
-                val currentTime = System.currentTimeMillis()
-                val timeBought = getCurrentTimeBoughtIap(context = context)
+            return if (getCurrentKeyIap(context).endsWith("0.3")) {
                 TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 1
-            }else if (getCurrentKeyIap(context).endsWith("0.5")) {
-                val currentTime = System.currentTimeMillis()
-                val timeBought = getCurrentTimeBoughtIap(context = context)
-                TimeUnit.MILLISECONDS.toDays(currentTime - timeBought) <= 2
+            } else if (getCurrentKeyIap(context).endsWith("0.5")) {
+                TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 2
             } else if (getCurrentKeyIap(context).endsWith("1")) {
-                TimeUnit.MILLISECONDS.toDays(currentTime - timeBought) <= 3
+                TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 3
             } else if (getCurrentKeyIap(context).endsWith("2")) {
-                val check = TimeUnit.MILLISECONDS.toDays(currentTime - timeBought) <= 6
+                val check = TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 6
                 check
             } else if (getCurrentKeyIap(context).endsWith("5")) {
-                TimeUnit.MILLISECONDS.toDays(currentTime - timeBought) <= 15
+                TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 15
             } else if (getCurrentKeyIap(context).endsWith("10")) {
-                TimeUnit.MILLISECONDS.toDays(currentTime - timeBought) <= 30
+                TimeUnit.MILLISECONDS.toMinutes(currentTime - timeBought) <= 30
             } else if (getCurrentKeyIap(context).endsWith("20")) {
                 true
             } else {
                 false
             }
         }
-
     }
 
-    private fun setCurrentTimeBoughtIap(context: Context) {
-        val time = System.currentTimeMillis()
-        val preferences =
-            context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-        preferences.edit().putLong("time_bought_iap", time).apply()
-    }
+        private fun setCurrentTimeBoughtIap(context: Context) {
+            val time = System.currentTimeMillis()
+            val preferences =
+                context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+            preferences.edit().putLong("time_bought_iap", time).apply()
+        }
 
 
-    private fun getCurrentTimeBoughtIap(context: Context): Long {
-        val preferences = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-        return preferences.getLong(
-            "time_bought_iap",
-            0L
-        )
-    }
+        private fun getCurrentTimeBoughtIap(context: Context): Long {
+            val preferences =
+                context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+            return preferences.getLong(
+                "time_bought_iap",
+                0L
+            )
+        }
 
-    private fun setCurrentKeyIap(context: Context, key: String) {
-        val preferences =
-            context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-        preferences.edit().putString("current_key_iap", key).apply()
-    }
+        private fun setCurrentKeyIap(context: Context, key: String) {
+            val preferences =
+                context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+            preferences.edit().putString("current_key_iap", key).apply()
+        }
 
-    private fun getCurrentKeyIap(context: Context): String {
-        val preferences =
-            context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-        return preferences.getString("current_key_iap", "").toString()
-    }
+        private fun getCurrentKeyIap(context: Context): String {
+            val preferences =
+                context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+            return preferences.getString("current_key_iap", "").toString()
+        }
 
-    @JvmStatic
-    fun isNetworkConnected(context: Context): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
-    }
+        @JvmStatic
+        fun isNetworkConnected(context: Context): Boolean {
+            val cm =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
+        }
 
-    @JvmStatic
-    fun formatPriceIap(p: String): String {
-        return try {
-            val str1 = p.trim().substring(0, 1)
-            str1.toInt()
-            val str = p.trim().substring(p.length - 1, p.length)
-            val price = p.substring(0, p.length - 1)
-            price + "" + str
-        } catch (e: Exception) {
-            val str = p.trim().substring(0, 1)
-            val price = p.substring(1, p.length)
-            price + "" + str
+        @JvmStatic
+        fun formatPriceIap(p: String): String {
+            return try {
+                val str1 = p.trim().substring(0, 1)
+                str1.toInt()
+                val str = p.trim().substring(p.length - 1, p.length)
+                val price = p.substring(0, p.length - 1)
+                price + "" + str
+            } catch (e: Exception) {
+                val str = p.trim().substring(0, 1)
+                val price = p.substring(1, p.length)
+                price + "" + str
+            }
         }
     }
-}
+
+
 
